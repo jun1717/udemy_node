e50d34fe84994dbd29e130950b3a3563cd74ed72
里屋純平 <satoya@satoyajunpei-no-MacBook-Pro.local>
2018-12-03 21:48:45 +0900

    install PassportJS

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //Strategy???
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
+
+// app.get('/', (req, res) => {
+//     res.send({ hi: 'there' });
+// });
+
 
 const PORT = process.env.PORT || 5000;
 
